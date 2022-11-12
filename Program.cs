c5d8bf42642563b1cdbbd3cfc0afab67c47b6c75
mohammedMesbahi <mohammed.mesbahiaouam@usmba.ac.ma>
2022-11-12 14:22:16 +0100

    add a new method to the calss Point

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TP03
+{
+    public class Point
+    {
+        private double x;
+        private double y;
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double GetX()
+        {
+            return x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+        public double ClaculateDistance(Point point)
+        {
+            double distance;
+            distance = Math.Sqrt(Math.Pow(this.x - point.x, 2) + Math.Pow(this.y - point.y, 2));
+            return distance;
+        }
+        public bool EqaulsTo(ref Point point)
+        {
+            return (this.x == point.x) && (this.y == point.y);
+        }
+        public Point Translation(double x = 0, double y = 0)
+        {
+            this.x += x;
+            this.y += y;
+            return this;
+        }
+        public bool Linearity(Point a, Point b)
+        {
+            //double determinant = (b.GetX() - this.GetX() * (c.GetY() - this.GetY()) - (c.GetY() - this.GetX()) * (b.GetY() - this.GetY()));
+            //return determinant == 0;
+            double area = x * (a.y - b.y) + a.x * (b.y - y) + b.x * (y - a.y);
+            return (area == 0);
+        }
+
+    }
+    class Exercise_1
+    {
+        public static void Main(string[] args)
+        {
+            Point p1 = new Point(2, 5);
+            Point p2 = new Point(3, 8);
+            Point p3 = new Point(1, 2);
+            Console.WriteLine("p1({0},{1}) ",p1.GetX(),p1.GetY());
+            Console.WriteLine("p2({0},{1}) ",p2.GetX(), p2.GetY());
+            Console.WriteLine("the distance between p1 and p2 : " + p1.ClaculateDistance(p2));
+            Console.Write("Linearity between p1({0},{1}) and p2({2},{3}) and p3({4},{5}) : ", p1.GetX(), p1.GetY(), p2.GetX(), p2.GetY(), p3.GetX(), p3.GetY());
+            if (p1.Linearity(p2,p3))
+            {
+                Console.WriteLine("true");
+
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("point")]
+[assembly: AssemblyTitle("TP03")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("point")]
+[assembly: AssemblyProduct("TP03")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26e5f6b0-cc7e-4dff-bb92-fbdb7c601712")]
+[assembly: Guid("384ed567-2dfa-44f6-a7fe-705a78befed5")]
 
 // Version information for an assembly consists of the following four values:
 //
